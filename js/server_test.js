1300b4976298fffd657f93b218b69cd5c26df37b
AlexT11223344 <alex8956123@gmail.com>
2022-09-07 15:20:39 -0500

    Update newest code for Scroll down

--- js/server_test.js
+++ js/server_test.js
@@ -15,84 +15,66 @@
 // })
 
 // Method 1.
-const http = require("http"), fs = require("fs"), url = require("url");
-const hostname = '127.0.0.1'
-const port = 3000
+// const http = require("http"), fs = require("fs"), url = require("url");
+// const hostname = '127.0.0.1'
+// const port = 3000
+//
+//
+// let server = http.createServer();
+// server.on("listening", function () {
+//     console.log(`Server running at http://${hostname}:${port}/`)
+// })
+//
+// server.on("request", function (req, res) {
+//
+//     res.writeHead(200, "OK", {'Content-Type': 'text/html'});
+//     // res.writeHead(200, "OK", {"Content-Type": 'text/css;charset=utf-8'});
+//     // res.writeHead(200, "OK", {"Content-Type": 'text/javascrpt;charset=utf-8'});
+//
+//     fs.readFile('../index.html', 'utf8', function (err, data) {
+//         if (err) throw err;
+//         res.end(data);
+//     });
+// })
+//
+// server.timeout = 1000;//5s response time
+// server.listen(port, hostname);//listing address - 127.0.0.1:3000
 
 
-let server = http.createServer();
-server.on("listening", function () {
-    console.log(`Server running at http://${hostname}:${port}/`)
+const http = require('http')
+const path = require('path')
+const fs = require('fs')
+
+const server = http.createServer()
+const port = 80
+const ip = '127.0.0.1'
+
+server.on('request', function(req, res) {
+    // Get url request from clint
+    const url = req.url
+    console.log(url)
+    let fpath_html = ''
+    let fpath_js = ''
+    let fpath_css = ''
+
+    //mapping the url to file path
+    if(url === '/'){
+        fpath_html = path.join(__dirname,'..','index.html')
+        console.log(fpath_html)
+
+        fpath_js = path.join(__dirname, url)
+        console.log(fpath_js)
+
+        fpath_css = path.join(__dirname,'..','css',url)
+        console.log(fpath_css)
+    }
+    else {
+        fpath_html = path.join(__dirname,'..','index.html')
+        fpath_js = path.join(__dirname, url)
+        fpath_css = path.join(__dirname,'..','css',url)
+    }
 })
 
-server.on("request", function (req, res) {
-
-    res.writeHead(200, "OK", {'Content-Type': 'text/html'});
-    // res.writeHead(200, "OK", {"Content-Type": 'text/css;charset=utf-8'});
-    // res.writeHead(200, "OK", {"Content-Type": 'text/javascrpt;charset=utf-8'});
-
-    fs.readFile('../index.html', 'utf8', function (err, data) {
-        if (err) throw err;
-        res.end(data);
-    });
+server.listen(port, function () {
+    console.log(`Server running at http://${ip}:${port}/`)
 })
-
-server.timeout = 1000;//5s response time
-server.listen(port, hostname);//listing address - 127.0.0.1:3000
-
-
-//Method 2.
-// let http = require("http");
-// let fs = require("fs");
-// let url = require("url");
-// let path = require("path");
-//
-// http.createServer((req, res) => {
-//
-//     let pathName = url.parse(req.url).pathname; //转换为url对象
-//
-//     //默认加载路径
-//     if(pathName == '/') {
-//         pathName = "/index.html";
-//     }
-//
-//     //获取文件后缀名
-//     let extName = path.extname(pathName);
-//
-//     // console.log("../fore_end" + pathName);  //例如../fore_end/css/style.css
-//     fs.readFile("../fore_end" + pathName, (err, data) => {
-//         if(err) { //出错则返回404页面
-//             console.log("404 Not Found!");
-//             fs.readFile("../fore_end/404.html", (errorNotFound, dataNotFound) => {
-//                 if(errorNotFound) {
-//                     console.log(errorNotFound);
-//                 }
-//                 else {
-//                     res.writeHead(200, {"Content-Type": "text/html;charset=utf-8"});
-//                     res.write(dataNotFound); //返回404页面
-//                     res.end();
-//                 }
-//             })
-//             return;
-//         }
-//         else {
-//             // 获取对应后缀的文件类型
-//             let ext = getExt(extName);
-//
-//             // 设置请求头
-//             res.writeHead(200, {"Content-Type": ext + "; charset=utf-8"});
-//             res.write(data); //返回请求文件
-//             res.end();
-//         }
-//     })
-// }).listen(8888);
-//
-// // 获取后缀名
-// getExt = (extName) => {
-//     switch(extName) {
-//         case '.html': return 'text/html';
-//         case '.css': return 'text/css';
-//         case '.js': return 'text/js';
-//         default: return 'text/html';
-//     }
-// }
